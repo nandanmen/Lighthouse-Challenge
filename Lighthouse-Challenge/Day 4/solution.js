641c23ff0862131568036b32bbdb3e674a4080c1
Narendras <n.syahrasyad.n@gmail.com>
2018-10-04 00:05:20 -0700

    Added day 4

---
+++ Lighthouse-Challenge/Day 4/solution.js
@@ -0,0 +1,9 @@
+const countRows = grid => grid.length;
+
+const countColumns = grid => grid.length ? grid[0].length : 0;
+
+const gridSize = grid => `${countColumns(grid)} x ${countRows(grid)}`;
+
+const totalCells = grid => countRows(grid) * countColumns(grid);
+
+module.exports = totalCells;
