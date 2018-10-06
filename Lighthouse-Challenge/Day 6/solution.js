1934f92ba11f78ab306dee9b18dc71c6120d30e8
Narendras <n.syahrasyad.n@gmail.com>
2018-10-06 14:21:45 -0700

    Added day 6

---
+++ Lighthouse-Challenge/Day 6/solution.js
@@ -0,0 +1,12 @@
+const convertColumn = coordinate => {
+    const colLetter = coordinate.subtr(0, 1).toLowerCase();
+    return colLetter.charCodeAt(0) - 97;
+}
+
+const lightCell = (grid, coordinate) => {
+    const row = coordinate.substr(1,1) - 1;
+    const col = convertColumn(coordinate);
+    return grid[row][col];
+}
+
+module.exports = lightCell; 
